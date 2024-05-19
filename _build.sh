dc17d75da29f8944a320d00163826c90257ffd09
Jason Pell <jason@pellcorp.com>
2024-05-19 18:24:52 +1000

    nerge from cryoz/main of build script and predefined .config files

---
+++ _build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# in case build is executed from outside current dir be a gem and change the dir
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
+cd $SCRIPT_DIR
+
+cp .config.$1 .config
+mkdir -p outfw
+make clean && make && mv out/klipper.bin outfw/${1}_klipper.bin
---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# in case build is executed from outside current dir be a gem and change the dir
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
+cd $SCRIPT_DIR
+
+./_build.sh bed
+./_build.sh noz
+./_build.sh mcu
